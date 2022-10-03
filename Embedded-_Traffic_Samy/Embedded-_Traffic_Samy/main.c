79a1fd76592666b4a9d767797c959c145f387475
msamy1197@gmail.com <msamy1197@gmail.com>
2022-10-03 17:17:13 +0200

    Creation of Project on ATMEL STUDIO

---
+++ Embedded-_Traffic_Samy/Embedded-_Traffic_Samy/main.c
@@ -0,0 +1,18 @@
+/*
+ * Embedded-_Traffic_Samy.c
+ *
+ * Created: 03/10/2022 05:16:35 م
+ * Author : mohsa
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
